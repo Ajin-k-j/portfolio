5d3de06fa2e4106cab1ef7631dff61e5fae4bba7
Ajin K J <ajin.kj@experionglobal.local>
2024-06-03 17:36:41 +0530

    mobile optimized with media query

--- script.js
+++ script.js
@@ -6,11 +6,10 @@
 //         customCursor.style.top = `${e.pageY}px`;
 //     });
 
-
 // mouse pointer animation
 const coords = { x: 0, y: 0 };
 const circles = document.querySelectorAll(".circle");
- 
+
 const colors = [
   "#ffb56b",
   "#fdaf69",
@@ -33,20 +32,20 @@ const colors = [
   "#680060",
   "#60005f",
   "#48005f",
-  "#3d005e"
+  "#3d005e",
 ];
- 
+
 circles.forEach(function (circle, index) {
   circle.x = 0;
   circle.y = 0;
   circle.style.backgroundColor = colors[index % colors.length];
 });
- 
-window.addEventListener("mousemove", function(e){
+
+window.addEventListener("mousemove", function (e) {
   coords.x = e.clientX;
   coords.y = e.clientY;
 });
- 
+
 function animateCircles() {
   let x = coords.x;
   let y = coords.y;
@@ -56,13 +55,12 @@ function animateCircles() {
     circle.style.scale = (circles.length - index) / circles.length;
     circle.x = x;
     circle.y = y;
- 
+
     const nextCircle = circles[index + 1] || circles[0];
     x += (nextCircle.x - x) * 0.3;
     y += (nextCircle.y - y) * 0.3;
   });
   requestAnimationFrame(animateCircles);
 }
- 
-animateCircles();
 
+animateCircles();
